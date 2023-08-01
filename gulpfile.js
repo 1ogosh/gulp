696f55c17bd0b882352bc9dfc8e27cd938057e29
Andrew Salmanov <ads739912@gmail.com>
2023-08-01 22:15:37 +0300

    min-js

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,14 @@ const { src, dest } = require('gulp');
 
 const scss = require('gulp-sass')(require('sass'));
 const concat = require('gulp-concat');
+const uglyfy = require('gulp-uglify-es').default;
+
+function scripts() {
+        return src('app/js/main.js')
+        .pipe(concat('main.min.js'))
+        .pipe(uglyfy())
+        .pipe(dest('app.js'))
+}
 function styles() {
         return src('app/scss/style.scss')
                 .pipe(concat('style.min.css'))
@@ -13,3 +21,4 @@ function styles() {
 }
 
 exports.styles = styles;
+exports.scripts = scripts;
